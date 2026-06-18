1fad38ad1a542a789df476ef81bc139cd6a00881
Md Mohibullah <mdmohibullah352@gmail.com>
2026-06-18 20:55:55 +0600

    added dom access

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Mohibullah");
+//DOM manipulation 
+// let abcd=document.getElementById("abcd");
+// console.log(abcd);
+// let h1= document.querySelector("h1");
+// h1.innerText = "mohibullah";
+
