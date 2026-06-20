701b3d559c9c7bf8a8b6e93ea341309aff0fad97
Md Mohibullah <mdmohibullah352@gmail.com>
2026-06-20 20:12:59 +0600

    added mini projects total characters in input handling

--- script.js
+++ script.js
@@ -44,5 +44,14 @@ let inp= document.querySelector("input");
 let span= document.querySelector("span");
 
 inp.addEventListener("input",function (){
-    span.textContent=inp.value.length;
+    // span.textContent=inp.value.length;
+    let left = 20 - inp.value.length;
+    span.textContent  = left;
+
+    if (left< 0){
+        span.style.color ="red";
+    }
+    else{
+        span.style.color ="black";
+    }
 });
